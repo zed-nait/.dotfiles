bd16a4aae25be420b40e67b190bf2f55396f5499
Akinshin Anton <akinshinao@protonmail.com>
2019-07-07 04:38:53 +0400

    moving to xfce4

--- scripts/.scripts/weather.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-string=`curl -s "http://wttr.in/Samara?m&format=%c+%t"`
-
-if [ $? != 0 ] || [[ $string == *"Internal Server Error"*  ]]; then 
-  echo "unknown"
-else
-  echo "$string"
-fi
